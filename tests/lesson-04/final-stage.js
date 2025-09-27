e3925858143e4ae4bbbcbb4a3f6f156d2cb26e96
Ngan Nguyen <82989993+NganNguyen224@users.noreply.github.com>
2025-09-27 09:29:24 +0700

    fix: Edit Challenge 05

--- tests/lesson-04/final-stage.js
+++ tests/lesson-04/final-stage.js
@@ -1,8 +1,8 @@
 function findPairsDivisibleBY17() {
     let count = 0;
     for (let i = 1; i <= 99; i++) {
-        for (let j = i+1; j <= 100; j++) {
-            let sum = i+j;
+        for (let j = i + 1; j <= 100; j++) {
+            let sum = i + j;
             if ((sum) % 17 === 0) {
                 console.log(`(${i}, ${j}) = ${sum}`);
                 count++;
